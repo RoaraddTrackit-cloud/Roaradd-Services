e296bcab6eaa59267dbf5b2a059938b338fc7686
kalyan modium <kalyan.modium@roaradd.com>
2026-03-21 21:53:58 -0400

    Create farm.tsx

---
+++ client/src/pages/farm.tsx
@@ -0,0 +1,12 @@
+export default function Farm() {
+  return (
+    <div style={{ width: "100%", height: "100vh", margin: 0, padding: 0 }}>
+      <iframe
+        src="/roaradd_farm.html"
+        title="RoarAdd LLC Circular Bio-Economy Farm"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        allow="fullscreen"
+      />
+    </div>
+  );
+}
