55bc8a53452a906fd2eee4128c0c74491872574a
kalyan modium <kalyan.modium@roaradd.com>
2026-07-26 22:48:50 -0400

    fix(spa): Include automatic 404.html SPA fallback in public/ directory and configure assetsDir

--- website/vite.github.config.ts
+++ website/vite.github.config.ts
@@ -21,8 +21,10 @@ export default defineConfig({
     dedupe: ["react", "react-dom"],
   },
   root: path.resolve(__dirname),
+  publicDir: path.resolve(__dirname, "public"),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
+    assetsDir: "assets",
   },
 });
